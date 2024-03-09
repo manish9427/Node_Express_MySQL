f2752c5a8794316101ad58b526f662127ca02521
Manish <manish119427@gmail.com>
2024-03-09 22:19:08 +0530

    new

--- db/db.js
+++ db/db.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const connection = () => {
-  mongoose
-    .connect(process.env.MONGODB_URL)
-    .then(() => {
-      console.log("Sucessfully Connected");
-    })
-    .catch((e) => {
-      console.log(e);
-    });
+const User = require("../model/userModel");
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Sucessfully Connected to MongoDB Atlas");
+  } catch (err) {
+    console.log("Error while connecting MongoDB Atlas", err);
+    process.exit(1);
+  }
 };
 
-module.exports = connection;
+module.exports = { connectDB, User };
--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
-const connection = require("./db/db");
-connection();
+const { connectDB, User } = require("././db/db");
+
+connectDB();
+app.get("/", (req, res) => {
+  res.send("Server is connected");
+});
 app.listen(port, () => {
-  console.log(port);
+  console.log(`Server is running http://localhost:3000/ on port ${port}`);
 });
