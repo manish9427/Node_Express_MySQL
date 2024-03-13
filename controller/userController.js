4d49a091248b1095bb02c003197de4dede8ae0ed
Manish <manish119427@gmail.com>
2024-03-13 23:38:45 +0530

    new

--- controller/userController.js
+++ controller/userController.js
@@ -19,4 +19,13 @@ const signup = async (req, res) => {
   }
 };
 
-module.exports = { signup };
+const getAllUser = async (req, res) => {
+  try {
+    const user = await User.find();
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { signup, getAllUser };
--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require("../controller/userController");
 
 router.post("/signup", userController.signup);
+router.get("/user", userController.getAllUser);
 
 module.exports = router;
