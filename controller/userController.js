f7f5810c17271f4825e51b896c943b275e93ff14
Manish <manish119427@gmail.com>
2024-03-23 23:42:34 +0530

    new

--- controller/userController.js
+++ controller/userController.js
@@ -1,65 +1,66 @@
 const bcrypt = require("bcryptjs");
 const User = require("../model/userModel");
 
-// const signup = async (req, res) => {
-//   try {
-//     const { username, password } = req.body;
-
-//     // Check if the username already exists
-//     const existingUser = await User.findOne({ username });
-
-//     if (existingUser) {
-//       // Username already exists, return an error response
-//       return res.status(400).json({ error: "Username is already taken" });
-//     }
-
-//     // Hash the password
-//     const hashedPassword = await bcrypt.hash(password, 10);
-
-//     // Create a new user
-//     const newUser = new User({
-//       username,
-//       password: hashedPassword,
-//     });
-
-//     // Save the new user to the database
-//     await newUser.save();
-
-//     // Return a success response
-//     res.status(201).json({ message: "User created successfully" });
-//   } catch (error) {
-//     // Handle any unexpected errors
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
 const signup = async (req, res) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  const isPresent = await User.findOne({ email });
-  if (isPresent) {
-    res.status(404);
-    res.send({ message: "Email Already registred" });
-  } else {
-    bcrypt.hash(password, 5, async function (err, hash) {
-      if (err) {
-        res
-          .status(404)
-          .send({ message: "Something went wrong please try later" });
-      }
+    // Check if the username already exists
+    const existingUser = await User.findOne({ name });
 
-      try {
-        const user = new UserModel({ name, email, password: hash });
-        await user.save();
-        res.send({ message: "Registration successful" });
-      } catch (error) {
-        res
-          .status(404)
-          .send({ message: "Something went wrong please try later" });
-      }
+    if (existingUser) {
+      // Username already exists, return an error response
+      return res.status(400).json({ error: "Username is already taken" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
     });
+
+    // Save the new user to the database
+    await newUser.save();
+
+    // Return a success response
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    // Handle any unexpected errors
+    res.status(500).json({ error: error.message });
   }
 };
+
+// const signup = async (req, res) => {
+//   const { name, email, password } = req.body;
+
+//   const isPresent = await User.findOne({ email });
+//   if (isPresent) {
+//     res.status(404);
+//     res.send({ message: "Email Already registred" });
+//   } else {
+//     bcrypt.hash(password, 5, async function (err, hash) {
+//       if (err) {
+//         res
+//           .status(404)
+//           .send({ message: "Something went wrong please try later" });
+//       }
+
+//       try {
+//         const user = new UserModel({ name, email, password: hash });
+//         await user.save();
+//         res.send({ message: "Registration successful" });
+//       } catch (error) {
+//         res
+//           .status(404)
+//           .send({ message: "Something went wrong please try later" });
+//       }
+//     });
+//   }
+// };
 
 // const getAllUser = async (req, res) => {
 //   try {
@@ -70,5 +71,5 @@ const signup = async (req, res) => {
 //   }
 // };
 
-module.exports = signup;
+module.exports = { signup };
 // module.exports = { signup, getAllUser };
